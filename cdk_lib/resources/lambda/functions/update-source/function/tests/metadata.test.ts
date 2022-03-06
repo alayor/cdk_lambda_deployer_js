e137d5db0123249c488eaf9b16d0ca7f0d182339
alayor <alayor3@gmail.com>
2022-03-06 10:03:46 -0700

    Add test to make sure a prod metadata updated from stage metadata.

--- cdk_lib/resources/lambda/functions/update-source/function/tests/metadata.test.ts
+++ cdk_lib/resources/lambda/functions/update-source/function/tests/metadata.test.ts
@@ -1,48 +1,69 @@
 import { s3 } from 'cdk_lib/_util/tests/mocking/aws_sdk' // this must be at the top
-import * as _ from 'lodash'
 import { when } from 'jest-when'
 import {
-    FUNCTIONS_METADATA_FILE_NAME,
-    LOCK_FILE,
-    PROD_BUCKET,
-    STAGE_BUCKET,
+  FUNCTIONS_METADATA_FILE_NAME,
+  LOCK_FILE,
+  PROD_BUCKET,
+  STAGE_BUCKET,
 } from '../src/constants'
 import { handler } from '../src/index'
 import {
-    whenS3GetObjectReturnsBody,
-    whenS3GetObjectThrowsError,
+  whenS3GetObjectReturnsBody,
+  whenS3GetObjectThrowsError,
 } from 'cdk_lib/_util/tests/mocking/s3'
 import { Metadata } from '../src/types'
 import { returnPromiseObject } from 'cdk_lib/_util/tests/mocking/promises'
 
-
 beforeEach(() => {
-    jest.clearAllMocks()
-    when(s3.getObject).mockImplementation(returnPromiseObject({}))
-    when(s3.putObject).mockImplementation(returnPromiseObject({}))
-    when(s3.copyObject).mockImplementation(returnPromiseObject({ VersionId: '1' }))
-    when(s3.deleteObject).mockImplementation(returnPromiseObject({ VersionId: '1' }))
-    whenS3GetObjectThrowsError({ Bucket: PROD_BUCKET, Key: LOCK_FILE }, { code: 'NoSuchKey' })
+  jest.clearAllMocks()
+  when(s3.getObject).mockImplementation(returnPromiseObject({}))
+  when(s3.putObject).mockImplementation(returnPromiseObject({}))
+  when(s3.copyObject).mockImplementation(returnPromiseObject({ VersionId: '1' }))
+  when(s3.deleteObject).mockImplementation(returnPromiseObject({ VersionId: '1' }))
+  whenS3GetObjectThrowsError({ Bucket: PROD_BUCKET, Key: LOCK_FILE }, { code: 'NoSuchKey' })
 })
 
 test('New Prod metadata is created from stage metadata.', async () => {
-//given
-    const metadata = require('./data/stageMetadata1.json') as Metadata
-    whenS3GetObjectReturnsBody(
-        { Bucket: STAGE_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
-        JSON.stringify(metadata),
-    )
-    whenS3GetObjectThrowsError(
-        { Bucket: PROD_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
-        { code: 'NoSuchKey' },
-    )
-    //when
-    await handler(null)
-    //then
-    const newProdMetadata = require('./data/newProdMetadataFromStage1.json') as Metadata
-    expect(s3.putObject).toBeCalledWith({
-        Bucket: PROD_BUCKET,
-        Key: FUNCTIONS_METADATA_FILE_NAME,
-        Body: JSON.stringify(newProdMetadata)
-    })
+  //given
+  const metadata = require('./data/stageMetadata1.json') as Metadata
+  whenS3GetObjectReturnsBody(
+    { Bucket: STAGE_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
+    JSON.stringify(metadata),
+  )
+  whenS3GetObjectThrowsError(
+    { Bucket: PROD_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
+    { code: 'NoSuchKey' },
+  )
+  //when
+  await handler(null)
+  //then
+  const newProdMetadata = require('./data/newProdMetadataFromStage1.json') as Metadata
+  expect(s3.putObject).toBeCalledWith({
+    Bucket: PROD_BUCKET,
+    Key: FUNCTIONS_METADATA_FILE_NAME,
+    Body: JSON.stringify(newProdMetadata),
+  })
+})
+
+test('Prod metadata is updated from stage metadata with new functions.', async () => {
+  //given
+  const stageMetadata = require('./data/stageMetadata1.json') as Metadata
+  whenS3GetObjectReturnsBody(
+    { Bucket: STAGE_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
+    JSON.stringify(stageMetadata),
+  )
+  const prodMetadata = require('./data/prodMetadata1.json') as Metadata
+  whenS3GetObjectReturnsBody(
+    { Bucket: PROD_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
+    JSON.stringify(prodMetadata),
+  )
+  //when
+  await handler(null)
+  //then
+  const newProdMetadata = require('./data/updatedProdMetadata1FromStage1.json') as Metadata
+  expect(s3.putObject).toBeCalledWith({
+    Bucket: PROD_BUCKET,
+    Key: FUNCTIONS_METADATA_FILE_NAME,
+    Body: JSON.stringify(newProdMetadata),
+  })
 })
