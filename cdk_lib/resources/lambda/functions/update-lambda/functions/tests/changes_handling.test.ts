3cb5de7c844c142bead0b004f826ca02eda0886a
alayor <alayor3@gmail.com>
2022-03-07 22:10:28 -0700

    Make sure we create lambda functions.

--- cdk_lib/resources/lambda/functions/update-lambda/functions/tests/changes_handling.test.ts
+++ cdk_lib/resources/lambda/functions/update-lambda/functions/tests/changes_handling.test.ts
@@ -32,20 +32,42 @@ test('Do not get metadata if changes summary has no changes.', async () => {
   })
 })
 
-test('create lambda function for each -create- action in the changes summary', async () => {
+test('Create lambda function for each -create- action in the changes summary', async () => {
   //given
   const summaryChanges = require('./data/summary_changes.json') as Metadata
   whenS3GetObjectReturnsBody(
-      { Bucket: PROD_BUCKET, Key: FUNCTIONS_CHANGES_SUMMARY_FILE_NAME },
-      JSON.stringify(summaryChanges),
+    { Bucket: PROD_BUCKET, Key: FUNCTIONS_CHANGES_SUMMARY_FILE_NAME },
+    JSON.stringify(summaryChanges),
   )
   const metadata = require('./data/metadata1.json') as Metadata
   whenS3GetObjectReturnsBody(
-      { Bucket: PROD_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
-      JSON.stringify(metadata),
+    { Bucket: PROD_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
+    JSON.stringify(metadata),
   )
   //when
   await handler(null)
   //then
-  expect(lambda.createFunction).toBeCalled()
+  const expectedKeysAndFunctionNames: Array<{ S3Key: string; FunctionName: string }> = [
+    {
+      S3Key: 'functions/customer/orders/place/function.zip',
+      FunctionName: 'api_customer_orders_place',
+    },
+    {
+      S3Key: 'functions/customer/products/get_all/function.zip',
+      FunctionName: 'api_customer_products_get_all',
+    },
+  ]
+  expectedKeysAndFunctionNames.forEach(({ S3Key, FunctionName }) => {
+    expect(lambda.createFunction).toBeCalledWith({
+      Code: {
+        S3Bucket: PROD_BUCKET,
+        S3Key,
+      },
+      FunctionName,
+      Role: '',
+      Handler: 'function.handler',
+      Runtime: 'nodejs14.x',
+      //TODO: Add Layer
+    })
+  })
 })
