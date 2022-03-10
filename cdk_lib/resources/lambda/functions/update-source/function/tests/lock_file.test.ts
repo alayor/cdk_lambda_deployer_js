330a7a3eede0050561ef95160daf43939817d9c1
alayor <alayor3@gmail.com>
2022-03-09 23:35:24 -0700

    Make sure a lock file is created when updating functions sources.

--- cdk_lib/resources/lambda/functions/update-source/function/tests/lock_file.test.ts
+++ cdk_lib/resources/lambda/functions/update-source/function/tests/lock_file.test.ts
@@ -1,4 +1,5 @@
-const { s3 } = require('cdk_lib/_util/tests/mocking/aws_sdk')
+import { s3 } from 'cdk_lib/_util/tests/mocking/aws_sdk' // this must be at the top
+import { Metadata } from '../src/types'
 import {
   FUNCTIONS_METADATA_FILE_NAME,
   LOCK_FILE,
@@ -7,20 +8,18 @@ import {
 } from '../src/constants'
 import { handler } from '../src'
 import {
+  whenS3GetObjectReturnsBody,
   whenS3GetObjectReturnsPromiseObject,
   whenS3GetObjectThrowsError,
 } from 'cdk_lib/_util/tests/mocking/s3'
+import { when } from 'jest-when'
+import { returnPromiseObject } from 'cdk_lib/_util/tests/mocking/promises'
 
 beforeEach(() => {
   jest.clearAllMocks()
-  whenS3GetObjectReturnsPromiseObject(
-    { Bucket: PROD_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
-    {},
-  )
-  whenS3GetObjectReturnsPromiseObject(
-    { Bucket: STAGE_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
-    {},
-  )
+  when(s3.getObject).mockImplementation(returnPromiseObject({}))
+  when(s3.putObject).mockImplementation(returnPromiseObject({}))
+  when(s3.copyObject).mockImplementation(returnPromiseObject({ VersionId: '1' }))
 })
 
 test('Do Not Get Metadata When Lock File Exists', async () => {
@@ -44,5 +43,26 @@ test('Get Metadata When Lock File Does Not Exist', async () => {
   expect(s3.getObject).toBeCalledWith({
     Bucket: STAGE_BUCKET,
     Key: FUNCTIONS_METADATA_FILE_NAME,
+  })
+})
+
+test('Create lock file.', async () => {
+  //given
+  const metadata = require('./data/metadata/stage1.json') as Metadata
+  whenS3GetObjectReturnsBody(
+    { Bucket: STAGE_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
+    JSON.stringify(metadata),
+  )
+  whenS3GetObjectThrowsError(
+    { Bucket: PROD_BUCKET, Key: FUNCTIONS_METADATA_FILE_NAME },
+    { code: 'NoSuchKey' },
+  )
+  //when
+  await handler(null)
+  //then
+  expect(s3.putObject).toBeCalledWith({
+    Bucket: PROD_BUCKET,
+    Key: LOCK_FILE,
+    Body: '',
   })
 })
