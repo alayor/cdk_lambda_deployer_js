e0c3f98dfee747cd44ceabb3111ffc38f8efbbec
alayor <alayor3@gmail.com>
2022-03-09 22:06:41 -0700

    Make sure layer source is updated if metadata hashes have changed.

--- cdk_lib/resources/lambda/layers/update_source/function/tests/layer_version_update.test.ts
+++ cdk_lib/resources/lambda/layers/update_source/function/tests/layer_version_update.test.ts
@@ -34,7 +34,7 @@ test('Do Not Update Layer Version If No Changes In Metadata.', async () => {
   expect(s3.copyObject).not.toBeCalled()
 })
 
-test('Update Layer Version If Changes In Metadata.', async () => {
+test('Update Layer Version If Changes In New Functions In Metadata.', async () => {
   //given
   const stageMetadata = require('./data/metadata/stage1.json') as Metadata
   whenS3GetObjectReturnsBody(
@@ -53,5 +53,27 @@ test('Update Layer Version If Changes In Metadata.', async () => {
     Bucket: PROD_BUCKET,
     Key: `libs/admin_lib/nodejs.zip`,
     CopySource: `${STAGE_BUCKET}/libs/admin_lib/nodejs.zip`,
+  })
+})
+
+test('Update Layer Version If Changes In Hashes Are Different.', async () => {
+//given
+  const stageMetadata = require('./data/metadata/stage1.json') as Metadata
+  whenS3GetObjectReturnsBody(
+      { Bucket: STAGE_BUCKET, Key: LIBS_METADATA_FILE_NAME },
+      JSON.stringify(stageMetadata),
+  )
+  const prodMetadata = require('./data/metadata/prod3.json') as Metadata
+  whenS3GetObjectReturnsBody(
+      { Bucket: PROD_BUCKET, Key: LIBS_METADATA_FILE_NAME },
+      JSON.stringify(prodMetadata),
+  )
+  //when
+  await handler(null)
+  //then
+  expect(s3.copyObject).toBeCalledWith({
+    Bucket: PROD_BUCKET,
+    Key: `libs/deliverer_lib/nodejs.zip`,
+    CopySource: `${STAGE_BUCKET}/libs/deliverer_lib/nodejs.zip`,
   })
 })
