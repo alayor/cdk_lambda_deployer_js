e3e6a572f6b841f99ced693e50b3043a889b4e62
alayor <alayor3@gmail.com>
2022-03-12 16:12:32 -0700

    Add s3 role permissions to lambda function.

--- cdk_lib/resources/lambda-functions/functions/update-source/index.ts
+++ cdk_lib/resources/lambda-functions/functions/update-source/index.ts
@@ -3,23 +3,58 @@ import { Construct } from 'constructs'
 import * as cdk from 'aws-cdk-lib'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'
-import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import MainConstruct, { MainConstructProps } from 'cdk_lib/context/main-construct'
+import { LambdaFunctionType, S3BucketType } from 'cdk_lib/context/resource-types'
+import * as iam from 'aws-cdk-lib/aws-iam'
 
-export type CdkLibProps = {
-  vpc: ec2.Vpc
-}
+export class UpdateSourceConstruct extends MainConstruct {
+  constructor(scope: Construct, id: string, props: MainConstructProps) {
+    super(scope, id, props)
+    const { context } = props
 
-export class UpdateSourceConstruct extends Construct {
-  constructor(scope: Construct, id: string, props: CdkLibProps) {
-    super(scope, id)
-    const { vpc } = props
-    new lambdaNodeJs.NodejsFunction(this, 'Function', {
+    const func = new lambdaNodeJs.NodejsFunction(this, 'Function', {
       runtime: lambda.Runtime.NODEJS_14_X,
       entry: path.join(__dirname, 'function/src/index.ts'),
       handler: 'handler',
       functionName: 'CdkLambdaDeployer_UpdateSource',
       timeout: cdk.Duration.minutes(10),
-      vpc,
+      vpc: context.getVpc(),
     })
+
+    const stageBucketArn = context.getS3Bucket(S3BucketType.STAGE).bucketArn
+    const prodBucketArn = context.getS3Bucket(S3BucketType.PROD).bucketArn
+
+    const rolePolicies = [
+      {
+        Effect: 'Allow',
+        Action: ['s3:ListBucket'], // Required to call listObjects
+        Resource: [stageBucketArn, prodBucketArn],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:GetObject'],
+        Resource: [prodBucketArn + '/updating_functions.lock'],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:GetObject'],
+        Resource: [stageBucketArn + '/functions/*', prodBucketArn + '/functions/*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:PutObject'],
+        Resource: [prodBucketArn + '/*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:DeleteObject'],
+        Resource: [prodBucketArn + '/functions/*'],
+      },
+    ]
+    rolePolicies.forEach((policy) => {
+      func?.role?.addToPrincipalPolicy(iam.PolicyStatement.fromJson(policy))
+    })
+
+    context.setLambdaFunction(LambdaFunctionType.DEPLOY_API_FUNCTIONS_UPDATE_SOURCE, func)
   }
 }
