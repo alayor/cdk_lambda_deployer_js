278d05369f2c996681a0edc44afdc4afe875b01c
alayor <alayor3@gmail.com>
2022-03-13 08:43:25 -0700

    Export Stack and Construct options to build the Cdk Lib.

--- cdk_lib/index.ts
+++ cdk_lib/index.ts
@@ -4,17 +4,32 @@ import Context from './context/index'
 import { AppName } from './context/app'
 import { S3BucketsConstruct } from './resources/s3-buckets/index'
 import { LambdaFunctionsConstruct } from './resources/lambda-functions'
+import { Stack } from 'aws-cdk-lib'
 
-export type CDKLambdaDeployerConstructProps = {
-  vpc: ec2.Vpc
+export type CDKLambdaDeployerVpcType = ec2.Vpc
+export type CDKLambdaDeployerConstructType = Construct
+
+export type CDKLambdaDeployerProps = {
+  vpc?: CDKLambdaDeployerVpcType
+  vpcId?: string
+}
+
+export class CDKLambdaDeployerStack extends Stack {
+  constructor(scope: Construct, id: string, props: CDKLambdaDeployerProps) {
+    super(scope, id)
+    if (!props.vpc && !props.vpcId) {
+      throw new Error('You have to specify either the vpc or vpcId param.')
+    }
+    new CDKLambdaDeployerConstruct(this, 'CDKLambdaDeployer', props)
+  }
 }
 
 export class CDKLambdaDeployerConstruct extends Construct {
-  constructor(scope: Construct, id: string, props: CDKLambdaDeployerConstructProps) {
+  constructor(scope: Construct, id: string, props: CDKLambdaDeployerProps) {
     super(scope, id)
-    const { vpc } = props
+    const { vpc, vpcId } = props
     const context = new Context(AppName.DEFAULT)
-    context.setVpc(vpc)
+    context.setVpc(vpc || (ec2.Vpc.fromLookup(this, 'Vpc', { vpcId }) as ec2.Vpc))
     new S3BucketsConstruct(this, 'S3Buckets', { context })
     new LambdaFunctionsConstruct(this, 'LambdaFunctions', { context })
   }
